f3681b0ed48d33a0ae9b00d671f9db053bd9fce8
Todor Ivanov <todor.ivanov@primeholding.com>
2026-01-14 18:05:29 +0200

    Fix the output path so Vercel does not get confused

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../"),
-
   /* config options here */
   transpilePackages: ["@ampeco/ampeco-ui"],
 
